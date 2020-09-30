3531b8ebac8ddd0dd31ea4392c7c06f3c8d8e880
François LaBerge <fgrclaberge@gmail.com>
2020-09-30 12:25:32 -0400

    Change plot to bargraph and edit gitignore of pycharm

--- a1/a1/plotDataset.py
+++ a1/a1/plotDataset.py
@@ -1,25 +1,31 @@
 import numpy as np
-import csv
 import matplotlib.pyplot as plt
 import argparse
 
-def main(csvDataset):
-    parser = argparse.ArgumentParser(description='Plot dataset')
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
 
-    dataset = np.loadtxt(csvDataset, delimiter=',')
-    plotDataset(dataset)
-    
-def plotDataset(dataset):
-    labels = [point[len(point)] for point in dataset]
-    buckets = np.zeros(max(labels))
-    for label in labes:
+def main(csv_dataset):
+    dataset = np.loadtxt(csv_dataset, delimiter=',', dtype=int)
+    plot_dataset(dataset)
+
+
+def plot_dataset(dataset):
+    labels = [point[len(point) - 1] for point in dataset]
+    buckets = np.zeros(max(labels) + 1)
+    for label in labels:
         buckets[label] += 1
-    plt.plot(np.arrange(buckets), buckets)
-    
-    
+    display_bargraph(np.arange(len(buckets)), buckets)
+
+
+def display_bargraph(categories, values):
+    plt.bar(categories, values)
+    plt.xticks(categories, categories)
+    plt.xlabel('Label ID')
+    plt.ylabel('# of occurrence in dataset')
+    plt.show()
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='plot a dataset')
     parser.add_argument('--dataset', metavar='path', required=True, help='the path to dataset')
+    args = parser.parse_args()
     main(args.dataset)
