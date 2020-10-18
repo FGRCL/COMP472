b7a39483f7ac22e8fe94365e7a49770b54bc7c57
Jacob Guirguis <jacob.guirguis@gmail.com>
2020-10-18 15:57:06 -0400

    Per-class metrics

--- a1/a1/main.py
+++ a1/a1/main.py
@@ -2,7 +2,7 @@ import argparse
 import sys
 from a1 import util
 from a1.modelenum import modeltype
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, precision_recall_fscore_support
 import pandas as pd
 
 def main(training_dataset, test_dataset, model_enum, output_file):
@@ -12,19 +12,57 @@ def main(training_dataset, test_dataset, model_enum, output_file):
     model = model_enum.create_model_lambda(training_dataset, features, labels)
     predictions = model.predict(input)
     confusion = confusion_matrix(expected_labels, predictions)
-    write_result_to_file(output_file, predictions, confusion)
+    metrics = precision_recall_fscore_support(expected_labels, predictions, average=None)
+
+    # Output
+    write_result_to_file(output_file, predictions, confusion, metrics)
 
 
-def write_result_to_file(output_file, predictions, confusion):
+def write_result_to_file(output_file, predictions, confusion, metrics):
+
+    # Predicted class
     for pair in zip(range(len(predictions)), predictions):
         print('{},{}'.format(pair[0], pair[1]), file=output_file)
-    print('confusion matrix:\n{}'.format(pd.DataFrame(confusion)), file=output_file)
+    
+    # Confusion Matrix
+    generate_confusion_matrix(output_file, confusion)
+
+    # Metrics
+    generate_metrics(metrics)
+
+def generate_confusion_matrix(output_file, confusion):
+    print('', file=output_file)
+    print('=================================', file=output_file)
+    print('CONFUSION MATRIX', file=output_file)
+    print('=================================', file=output_file)
+    print('', file=output_file)
+
+    pd.DataFrame(confusion).to_csv(output_file, mode='a', header='false')
+
+def generate_metrics(metrics):
+    print('', file=output_file)
+    print('=================================', file=output_file)
+    print('TESTING', file=output_file)
+    print('=================================', file=output_file)
+    print('', file=output_file)
+
+    metric_type = ["PRECISION", "RECALL", "F-MEASURE", "SUPPORT"]
+
+    for index, metric in enumerate(metrics):
+            print('', file=output_file)
+            print('=================================', file=output_file)
+            print(metric_type[index], file=output_file)
+            print('=================================', file=output_file)
+            print('', file=output_file)
+            for pair in zip(range(len(metric)), metric):
+                print('{},{}'.format(pair[0], pair[1]), file=output_file)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='Model trainer', description='An AI model trainer')
     parser.add_argument('--trainingset', '-tr', metavar='training set path', required=True, help='the path to training set', )
     parser.add_argument('--testset', '-ts', metavar='test set path', required=True, help='the path to test set', )
-    parser.add_argument('--output', '-o', metavar='the path to the ouput file', required=False, help='the path to training set', default=sys.stdout)
+    parser.add_argument('--output', '-o', metavar='the path to the ouput file', required=False, help='the path to output file', default=sys.stdout)
     parser.add_argument('-m', '--model',
         type=modeltype.from_string,
         choices=modeltype,
